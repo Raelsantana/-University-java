99b8b519ffefd03afb6e722e6516d465ee43af6b
Raelsantana <raelsantana1@hotmail.com>
2025-04-11 17:51:02 -0300

    Criando os arquivos base para o sistema

---
+++ src/edu/universidade/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    // Este é o ponto de entrada do programa
+    // Aqui você pode inicializar o sistema de gerenciamento de universidade
+    // e chamar os métodos necessários para iniciar o aplicativo.
+
+public static void main(String[] args) {
+    System.out.println("Iniciando o sistema de gerenciamento de universidade...");
+}
+}
